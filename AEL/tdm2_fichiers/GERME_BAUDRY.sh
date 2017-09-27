891a814c857ac7d640e2ae9eb38d5ccb7bfff3e3
Robin Germe <germerobin@gmail.com>
2017-09-27 20:47:28 +0200

    AEL

--- AEL/tdm2_fichiers/GERME_BAUDRY.sh
+++ AEL/tdm2_fichiers/GERME_BAUDRY.sh
@@ -22,10 +22,10 @@ echo "** EXO 2"
 echo "** Q1 :"
 valeurAtribut='"([^"<&]|&[a-ZA-Z:_][a-zA-Z0-9:_.-]*;)*'
 
-egrep --color=auto $valeurAtribut contact.html
+egrep -m 10 --color=auto $valeurAtribut html/contact.html
 
 echo "** Q2 :"
 
 nomXML='[a-zA-Z:_][a-zA-Z0-9:_.-]*'
 
-#egrep --color=auto $nomXML contact.html
+#egrep -m 10 --color=auto $nomXML html/contact.html
